683f84ff95f746faa4cc6a6ee712b13361c77d70
gabriel-intel <gabriel.l.silva@intel.com>
2022-11-08 18:22:13 -0700

    Download DLStreamer pipeline zoo models and videos

---
+++ install-artifacts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt update
+DEBIAN_FRONTEND=noninteractive apt install wget -y
+mkdir -p vids
+mkdir -p models
+mkdir -p scripts
+cd models
+wget https://raw.githubusercontent.com/dlstreamer/pipeline-zoo-models/main/storage/yolov5s-416_INT8/yolov5s.json
+wget https://raw.githubusercontent.com/dlstreamer/pipeline-zoo-models/main/storage/yolov5s-416_INT8/FP32-INT8/yolov5s.xml
+wget https://github.com/dlstreamer/pipeline-zoo-models/raw/main/storage/yolov5s-416_INT8/FP32-INT8/yolov5s.bin
+wget https://raw.githubusercontent.com/dlstreamer/pipeline-zoo-models/main/storage/efficientnet-b0_INT8/efficientnet-b0.json
+wget https://raw.githubusercontent.com/dlstreamer/pipeline-zoo-models/main/storage/efficientnet-b0_INT8/FP32-INT8/efficientnet-b0.xml
+wget https://github.com/dlstreamer/pipeline-zoo-models/raw/main/storage/efficientnet-b0_INT8/FP32-INT8/efficientnet-b0.bin
+cd ../vids
+wget https://github.com/intel-iot-devkit/sample-videos/raw/master/bottle-detection.mp4
+cd ..
+
